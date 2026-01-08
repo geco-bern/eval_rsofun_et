ed5a3703ec7a69e728c7a2365db11fbb217860f3
Beni Stocker <benjamin.stocker@gmail.com>
2026-01-08 12:05:03 +0100

    added drought response evaluation

--- analysis/03_fluxnet_eval_plot.R
+++ analysis/03_fluxnet_eval_plot.R
@@ -14,7 +14,6 @@ library(ggthemes)
 library(purrr)
 library(here)
 library(see)
-# library(ncdf4)
 library(cowplot)
 library(here)
 
@@ -113,6 +112,18 @@ out_eval_pm_s0 <- eval_sofun(
   light = FALSE
 )
 
+#### run drought response evaluation --------
+df_droughtresponse_pm_s0 <- eval_droughtresponse(
+  df = out_eval_pm_s0$le$fluxnet$data$ddf %>%
+    rename(site = sitename),
+  df_flue = readr::read_csv("data/fluxnet/flue_stocker18nphyt.csv"),
+  before = 20,
+  after = 105,
+  leng_threshold = 10,
+  nbins = 10,
+  do_norm = TRUE
+)
+
 ### PM ---------------
 #### rsofun driver object ---------------
 driver_pm <- read_rds(here("data/driver_pm.rds")) # created in 01_calib_fluxnet.R
@@ -124,7 +135,7 @@ par_calib <- read_rds(here("data/calib_output_pm.rds"))
 params_modl <- c(par_calib$par[c("kphio", "kphio_par_a", "kphio_par_b", "soilm_thetastar", "gw_calib")], params_fix)
 
 #### run model ---------------
-output_pt <- runread_pmodel_f(
+output_pm <- runread_pmodel_f(
   driver_pm,
   par = params_modl
 )
@@ -137,23 +148,34 @@ settings_eval <- list(
 )
 
 out_eval_pm <- eval_sofun(
-  output_pt,
+  output_pm,
   settings_eval,
   obs_eval = obs_eval,
   overwrite = TRUE,
   light = FALSE
 )
 
+#### run drought response evaluation --------
+df_droughtresponse_pm <- eval_droughtresponse(
+  df = out_eval_pm$le$fluxnet$data$ddf %>%
+    rename(site = sitename),
+  df_flue = readr::read_csv("data/fluxnet/flue_stocker18nphyt.csv"),
+  before = 20,
+  after = 105,
+  leng_threshold = 10,
+  nbins = 10,
+  do_norm = TRUE
+)
 
 ### PT ---------------
 #### rsofun driver object ---------------
 driver_pt <- read_rds(here("data/driver_pt.rds")) # created in 01_calib_fluxnet.R
 
 #### calibrated parameters ---------------
-par_calib <- read_rds(here("data/fluxnet/global_calib_PT.rds"))
+par_calib <- read_rds(here("data/calib_output_pt.rds"))
 
 #### construct all parameters ---------------
-params_modl <- c(par_calib, par_fixed)
+params_modl <- c(par_calib$par[c("kphio", "kphio_par_a", "kphio_par_b", "soilm_thetastar", "gw_calib")], params_fix)
 
 #### run model ---------------
 output_pt <- runread_pmodel_f(
@@ -168,7 +190,7 @@ settings_eval <- list(
   agg = 8
 )
 
-out_eval <- eval_sofun(
+out_eval_pt <- eval_sofun(
   output_pt,
   settings_eval,
   obs_eval = obs_eval,
@@ -176,13 +198,30 @@ out_eval <- eval_sofun(
   light = FALSE
 )
 
+#### run drought response evaluation --------
+df_droughtresponse_pt <- eval_droughtresponse(
+  df = out_eval_pt$le$fluxnet$data$ddf %>%
+    rename(site = sitename),
+  df_flue = readr::read_csv("data/fluxnet/flue_stocker18nphyt.csv"),
+  before = 20,
+  after = 105,
+  leng_threshold = 10,
+  nbins = 10,
+  do_norm = TRUE
+)
+
 ## Create plots --------
 ### Mod vs Obs, x-daily LE ------------
-out_eval_pm_s0$le$fluxnet$plot$gg_modobs_monthly +
+gg1 <- out_eval_pm_s0$le$fluxnet$plot$gg_modobs_monthly +
   labs(title = NULL)
 
-out_eval_pm$le$fluxnet$plot$gg_modobs_monthly +
+gg2 <- out_eval_pm$le$fluxnet$plot$gg_modobs_monthly +
   labs(title = NULL)
+
+gg3 <- out_eval_pt$le$fluxnet$plot$gg_modobs_monthly +
+  labs(title = NULL)
+
+cowplot::plot_grid(gg1, gg2, gg3, labels = letters[1:3], ncol = 3)
 
 ### Mean seasonal cycle by climate zone -----------
 # number of sites per climate zone
@@ -208,11 +247,19 @@ out_eval_pm_s0$le$fluxnet$data$meandoydf_byclim %>%
       select(-starts_with("obs")),
     by = join_by(climatezone, doy)
   ) |>
+  left_join(
+    out_eval_pt$le$fluxnet$data$meandoydf_byclim %>%
+      filter(nsites >= 5) |>
+      rename_with(~ sub("^mod_", "mod_pt_", .x), starts_with("mod_")) |>
+      select(-starts_with("obs")),
+    by = join_by(climatezone, doy)
+  ) |>
   pivot_longer(
-    cols = c(obs_mean, mod_pm_s0_mean, mod_pm_mean),
+    cols = c(obs_mean, mod_pm_s0_mean, mod_pm_mean, mod_pt_mean),
     names_to = "source",
     values_to = "le"
   ) %>%
+  mutate(source = factor(source, levels = c("obs_mean", "mod_pt_mean", "mod_pm_mean", "mod_pm_s0_mean"))) |>
   ggplot() +
   geom_ribbon(
     aes(x = doy, ymin = obs_min, ymax = obs_max),
@@ -226,34 +273,51 @@ out_eval_pm_s0$le$fluxnet$data$meandoydf_byclim %>%
   facet_wrap(~climatezone, ncol = 4) +
   theme_bw() +
   theme(
-    legend.position = "bottom",
-    strip.text = element_text(size = 14)
+    legend.position = "bottom"
+    # strip.text = element_text(size = 14)
   ) +
   scale_color_manual(
     name = NULL,
-    breaks = c("PM" = "mod_pm_mean", "PM-S0" = "mod_pm_s0_mean", "Observed" = "obs_mean"),
-    values = c(unname(see::okabeito_colors()[c(1,5)]), "black")
+    breaks = c("PM" = "mod_pm_mean", "PM-S0" = "mod_pm_s0_mean", "PT" = "mod_pt_mean", "Observed" = "obs_mean"),
+    values = c(unname(see::okabeito_colors()[c(2, 5, 7)]), "black")
   )
 
 #### GPP ---------
-zones <- out_eval_pm_s0$le$fluxnet$data$meandoydf_byclim %>%
-  filter(nsites >= 5) |>
-  pull(climatezone) |>
-  unique()
-
 out_eval_pm_s0$gpp$fluxnet$data$meandoydf_byclim %>%
-  dplyr::filter(climatezone %in% zones) %>%
+  filter(nsites >= 5) |>
+  # dplyr::filter(climatezone %in%
+  #   c(
+  #     "Csb north", "Csa north", "ET north",
+  #     "Dfb north", "Dsa north", "Af north",
+  #     "Aw south", "Cfb north", "Bsh south"
+  #   )) %>%
+  rename_with(~ sub("^mod_", "mod_pm_s0_", .x), starts_with("mod_")) |>
+  left_join(
+    out_eval_pm$gpp$fluxnet$data$meandoydf_byclim %>%
+      filter(nsites >= 5) |>
+      rename_with(~ sub("^mod_", "mod_pm_", .x), starts_with("mod_")) |>
+      select(-starts_with("obs")),
+    by = join_by(climatezone, doy)
+  ) |>
+  left_join(
+    out_eval_pt$gpp$fluxnet$data$meandoydf_byclim %>%
+      filter(nsites >= 5) |>
+      rename_with(~ sub("^mod_", "mod_pt_", .x), starts_with("mod_")) |>
+      select(-starts_with("obs")),
+    by = join_by(climatezone, doy)
+  ) |>
   pivot_longer(
-    c(obs_mean, mod_mean),
+    cols = c(obs_mean, mod_pm_s0_mean, mod_pm_mean, mod_pt_mean),
     names_to = "source",
     values_to = "gpp"
   ) %>%
+  mutate(source = factor(source, levels = c("obs_mean", "mod_pt_mean", "mod_pm_mean", "mod_pm_s0_mean"))) |>
   ggplot() +
   geom_ribbon(
     aes(x = doy, ymin = obs_min, ymax = obs_max),
     fill = "grey70"
   ) +
-  geom_line(aes(x = doy, y = gpp, color = source), size = 0.4) +
+  geom_line(aes(x = doy, y = gpp, color = source)) +
   labs(
     y = expression(paste("GPP  (gC m"^-2, "d"^-1, ")")),
     x = "DOY"
@@ -261,14 +325,127 @@ out_eval_pm_s0$gpp$fluxnet$data$meandoydf_byclim %>%
   facet_wrap(~climatezone, ncol = 4) +
   theme_bw() +
   theme(
-    legend.position = "bottom",
-    strip.text = element_text(size = 14)
+    legend.position = "bottom"
+    # strip.text = element_text(size = 14)
   ) +
   scale_color_manual(
     name = NULL,
-    breaks = c("Modelled" = "mod_mean", "Observed" = "obs_mean"),
-    values = c("red", "black")
+    breaks = c("PM" = "mod_pm_mean", "PM-S0" = "mod_pm_s0_mean", "PT" = "mod_pt_mean", "Observed" = "obs_mean"),
+    values = c(unname(see::okabeito_colors()[c(2, 5, 7)]), "black")
   )
+
+### Drought response ------
+gg1 <- df_droughtresponse_pm_s0 %>%
+  ggplot(aes(x = dday)) +
+  geom_hline(
+    yintercept = 0,
+    color = "black",
+    linetype = "dotted"
+  ) +
+  geom_vline(
+    xintercept = 0,
+    color = "black",
+    linetype = "dotted"
+  ) +
+  geom_line(
+    aes(y = median),
+    size = 0.9
+  ) +
+  geom_ribbon(
+    aes(ymin = q33, ymax = q66),
+    alpha = 0.3
+  ) +
+  scale_x_continuous(
+    expand = c(0, 0),
+    limits = c(-20, 105)
+  ) +
+  scale_y_continuous(
+    expand = c(0, 0),
+    limits = c(-40, 20)
+  ) +
+  labs(
+    title = "PM-S0",
+    x = "Days after drought onset",
+    y = expression(paste("Bias (W m"^{-2}, ")"))
+  ) +
+  theme_classic()
+
+gg2 <- df_droughtresponse_pm %>%
+  ggplot(aes(x = dday)) +
+  geom_hline(
+    yintercept = 0,
+    color = "black",
+    linetype = "dotted"
+  ) +
+  geom_vline(
+    xintercept = 0,
+    color = "black",
+    linetype = "dotted"
+  ) +
+  geom_line(
+    aes(y = median),
+    size = 0.9
+  ) +
+  geom_ribbon(
+    aes(ymin = q33, ymax = q66),
+    alpha = 0.3
+  ) +
+  scale_x_continuous(
+    expand = c(0, 0),
+    limits = c(-20, 105)
+  ) +
+  scale_y_continuous(
+    expand = c(0, 0),
+    limits = c(-40, 20)
+  ) +
+  labs(
+    title = "PM",
+    x = "Days after drought onset",
+    y = expression(paste("Bias (W m"^{-2}, ")"))
+  ) +
+  theme_classic()
+
+gg3 <- df_droughtresponse_pt %>%
+  ggplot(aes(x = dday)) +
+  geom_hline(
+    yintercept = 0,
+    color = "black",
+    linetype = "dotted"
+  ) +
+  geom_vline(
+    xintercept = 0,
+    color = "black",
+    linetype = "dotted"
+  ) +
+  geom_line(
+    aes(y = median),
+    size = 0.9
+  ) +
+  geom_ribbon(
+    aes(ymin = q33, ymax = q66),
+    alpha = 0.3
+  ) +
+  scale_x_continuous(
+    expand = c(0, 0),
+    limits = c(-20, 105)
+    ) +
+  scale_y_continuous(
+    expand = c(0, 0),
+    limits = c(-40, 20)
+    ) +
+  labs(
+    title = "PT",
+    x = "Days after drought onset",
+    y = expression(paste("Bias (W m"^{-2}, ")"))
+  ) +
+  theme_classic()
+
+cowplot::plot_grid(
+  gg1, gg2, gg3,
+  labels = letters[1:3],
+  ncol = 3
+)
+
 
 
 # OLD -------------------------
@@ -606,7 +783,6 @@ so_fun_analysis <- function(out_dir, prefix) {
 
 
   # GPP drought bias
-
   df_dday_agg <- eval_droughtresponse(
     df = out_eval$gpp$fluxnet$data$ddf %>%
       rename(
